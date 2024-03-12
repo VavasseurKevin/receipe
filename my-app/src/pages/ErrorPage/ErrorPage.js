d952ba784562150d37aa8f480474b0cf2f76a09e
VavasseurKevin <minhluan.phan@gmail.com>
2024-03-12 22:42:59 +0100

    Create pages folder with CategoryPage/ErrorPage/Homepage/MealDetailsPage in src

---
+++ my-app/src/pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./ErrorPage.scss";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ErrorPage;
---
+++ my-app/src/pages/MealDetailsPage/MealDetailsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./MealDetailsPage.scss";
+
+const MealDetailsPage = () => {
+
+  return (
+    <main >
+    </main>
+  )
+}
+
+export default MealDetailsPage;
