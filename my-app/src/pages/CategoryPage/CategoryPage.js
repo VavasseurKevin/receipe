c062c879c30945f7b470f62014425ebb40a915a3
VavasseurKevin <minhluan.phan@gmail.com>
2024-03-18 22:16:55 +0100

    Create utils folder in src with images.js

---
+++ my-app/src/pages/CategoryPage/CategoryPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CategoryPage.scss";
+
+const CategoryPage = () => {
+    return (
+        <main>
+
+        </main>
+    )
+};
+
+export default CategoryPage;
